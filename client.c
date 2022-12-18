3afeffb32c350fbd45d943e68246b95c8a0024fc
Raphael <rzaccari@student.42lauanne.ch>
2022-12-18 05:15:08 +0100

    programme fonctionnel non normé

--- client.c
+++ client.c
@@ -4,31 +4,75 @@
 #include <stdlib.h>
 #include "./libft/libft.h"
 
-int	main(int ac, char **av)
+void	send_pid(pid_t server_pid)
+{
+	int		i;
+	pid_t	pid;
+
+	pid = getpid();
+	i = 32;
+	while (i)
+	{
+		--i;
+		//printf("%d\n", i);
+		if (pid & ((unsigned int)1 << i))
+			kill(server_pid, SIGUSR1);
+		else
+			kill(server_pid, SIGUSR2);
+		usleep(200);	
+	}
+}
+
+void	check_ok(int sig)
+{
+	if (sig == SIGUSR1)
+		write(1, "Message transmis\n", 17);
+}
+
+void	send_eol(pid_t pid)
 {
 	int	i;
-	int j;
+
+	i = 8;
+	while (i)
+	{
+		--i;
+		kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	int 	j;
 	pid_t	pid;
 	char	c;
+
 	if (ac < 3)
 		return (1);
-		pid = atoi(av[1]);
+	signal(SIGUSR1, check_ok);
+	signal(SIGUSR2, check_ok);
+	pid = ft_atoi(av[1]);
+	send_pid(pid);
 	i = 0;
 	while (av[2][i])
 	{
-		j = 0;
+		j = 8;
 		c = av[2][i];
-		while (j < 8)
+		while (j)
 		{
-			if (c & (unsigned char)1)
-				kill(pid, SIGUSR2);
-			else
+			--j;
+			if (c & ((unsigned char)1 << j))
 				kill(pid, SIGUSR1);
-			++j;
-			c >>= 1;
-			usleep(200);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+			
 		}
 		++i;
 	}
+	send_eol(pid);
+	usleep(100);
 	return (0);
 }
--- server.c
+++ server.c
@@ -11,12 +11,90 @@ int	displayPid()
 	return (1);
 }
 
+pid_t	get_pid(int sig, int check_pid)
+{
+	static pid_t	pid;
+	static int		i;
+
+	if (!check_pid)
+		pid = 0;
+	pid <<= 1U;
+	if (sig == SIGUSR1)
+	{
+		pid = pid | (unsigned int)1;
+	}
+	++i;
+	return (pid);
+}
+
+char	*strdup_plus(char *s)
+{
+	char	*ret;
+	int	i;
+
+	ret = ft_calloc(ft_strlen(s) + 1001, sizeof(char));
+	if (!ret)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		ret[i] = s[i];
+		i++;
+	}
+	free(s);
+	return (ret);
+}
+
 void	displayCharacter(int sig)
 {
+	static char			*s;
+	static int			i;
+	static int			j;
+	static int			size;
+	static pid_t		pid;
+	static int			check_pid;
+
+	if (check_pid < 32)
+	{
+		pid = get_pid(sig, check_pid);
+		++check_pid;
+		return ;
+	}
+	if (!s)
+	{
+		s = ft_calloc(1001, sizeof(char));
+		if (!s)
+			return ;
+	}
+	s[j] <<= 1;
 	if (sig == SIGUSR1)
-		write(1, "0", 1);
+		s[j] = s[j] | (unsigned char)1;
+	if (i == 7)
+	{
+		if (!s[j])
+		{
+			kill(pid, SIGUSR1);
+			check_pid = 0;
+			write(1, s, ft_strlen(s));
+			free(s);
+			s = NULL;
+			j = 0;
+			i = 0;
+			return ;
+		}
+		i = 0;
+		++j;
+		if (j >= (1000 * size) - 1)
+		{
+			size++;
+			s = strdup_plus(s);
+			if (!s)
+				return ;
+		}
+		s[j] = 0;
+	}
 	else
-		write(1, "1", 1);
+		++i;
 }
 
 int	main(int argc, char *argv[])
